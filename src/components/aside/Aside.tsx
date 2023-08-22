7acdc153f6ade63937e8cfbeed600b2388a361f0
Emanuele Pavan <emanuele.pav@gmail.com>
2023-08-22 19:32:12 +0200

    resizes logo based on container inline size

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -9,7 +9,7 @@ export default function Aside() {
     <aside className={styles.mpAside}>
       <header className={styles.mpAsideHeader}>
         <img src={logo} className={styles.mpLogo} alt="logo music player" />
-        <h1>Library</h1>
+        <h1 className={styles.mpTitle}>Library</h1>
       </header>
       <ul className={styles.mpAsideList}>
         {songs.map((song: Song) => (
