37d8d6c09b620333672095c4b1cb6fa2e3c8ab79
Emanuele Pavan <emanuele.pav@gmail.com>
2023-08-22 15:43:21 +0200

    drops webVitals for the sake of this test

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,6 @@ import './index.module.css';
 
 import App from './App';
 
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -18,8 +16,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
